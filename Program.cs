37e8d7e547738b11f4cfe9236dafe32c7a3395d0
Andy Porras <andyporras@estudiantec.cr>
2022-08-24 12:57:28 -0600

    Archivo principal

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Agregar servicios al contenedor 
+builder.Services.AddControllersWithViews();
+
+var aplicacion = builder.Build();
+
+// Configuracion el HTTP
+if (!aplicacion.Environment.IsDevelopment())
+{
+    aplicacion.UseExceptionHandler("/Home/Error");
+    aplicacion.UseHsts();
+}
+aplicacion.UseHttpsRedirection();
+aplicacion.UseStaticFiles();
+aplicacion.UseRouting();
+aplicacion.UseAuthorization();
+aplicacion.MapControllerRoute(name: "default", pattern: "{controller = Home}/{action = Index}/{id?}");
+aplicacion.Run();
